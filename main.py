d55662817e4e4a2f9591ad8e8a2ae40cc0dceb9d
Agócs Dániel <agocsdaniel@gmail.com>
2022-03-17 22:46:09 +0100

    Only notify when orderable

--- main.py
+++ main.py
@@ -81,12 +81,15 @@ while True:
         doc = BeautifulSoup(requests.get('https://schpincer.sch.bme.hu/').content, 'html.parser')
         openings = []
         for tr in doc.find(class_='circles-table').find_all('tr'):
+            circle_id = str(tr.find(class_='arrow').find('a')['href']).split('/')[2]
+            items = list(requests.get('https://schpincer.sch.bme.hu/api/items/?circle=' + circle_id).json())
             openings.append({
                 'name': tr.find('a').contents[0],
                 'circleUrl': 'https://schpincer.sch.bme.hu' + tr.find('a')['href'],
                 'circleColor': tr['class'][0],
                 'openingStart': int(datetime.datetime.strptime(tr.find(class_='date').contents[0], '%H:%M (%y-%m-%d)').timestamp())*1000,
-                'feeling': dict(enumerate(tr.find(class_='feeling').contents)).get(0)
+                'feeling': dict(enumerate(tr.find(class_='feeling').contents)).get(0),
+                'orderable': any(list(map(lambda a: a['orderable'] and not a['outOfStock'], items)))
             })
     except:
         logging.error('Problem while fetching pincer api')
@@ -94,7 +97,7 @@ while True:
         continue
 
     for opening in openings:
-        if not cur.execute("select * from sent where id=:id", {"id": opening['openingStart']}).fetchone():
+        if opening['orderable'] and not cur.execute("select * from sent where id=:id", {"id": opening['openingStart']}).fetchone():
             if opening['name'] == 'Vödör' or 'vodor' in opening['circleUrl']:
                 content = simple_template % {
                     'message': "FYI, @&" + os.environ['MENTIONED_ROLE'],
